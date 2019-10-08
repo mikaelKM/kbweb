31d78b33c38e9c95ace16620718d58b05a299141
mikaelKM <mikaelkasike861@gmail.com>
2019-10-08 12:27:21 +0300

    Connected the project to mysql database

--- mikaelweb/settings.py
+++ mikaelweb/settings.py
@@ -73,14 +73,26 @@ WSGI_APPLICATION = 'mikaelweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+#
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
+        #'NAME': 'django_blog',
+        #'USER': 'joginder',
+        #'PASSWORD': 'joginder',
+        #'HOST': 'localhost',
+        #'PORT': '3306',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
